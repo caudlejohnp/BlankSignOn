cae758c06d7cc8ee20272d3d5575f055df62e39c
caudlejohnp <caudlejohnp@gmail.com>
2020-09-19 19:01:53 -0500

    added routing after registering

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,58 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"],
 })
 export class RegistrationComponent implements OnInit {
-
   formSubmit: FormGroup;
 
   get inputEmailInvalid(): boolean {
-    return !this.formSubmit.controls.inputEmail.valid &&
-      this.formSubmit.controls.inputEmail.touched;
+    return (
+      !this.formSubmit.controls.inputEmail.valid &&
+      this.formSubmit.controls.inputEmail.touched
+    );
   }
 
   get inputPasswordInvalid(): boolean {
-    return !this.formSubmit.controls.inputPassword.valid &&
-      this.formSubmit.controls.inputPassword.touched;
+    return (
+      !this.formSubmit.controls.inputPassword.valid &&
+      this.formSubmit.controls.inputPassword.touched
+    );
   }
 
   get confirmPasswordInvalid(): boolean {
-    return !this.formSubmit.controls.confirmPassword.valid &&
-      this.formSubmit.controls.confirmPassword.touched;
+    return (
+      !this.formSubmit.controls.confirmPassword.valid &&
+      this.formSubmit.controls.confirmPassword.touched
+    );
   }
 
   get passwordError(): boolean {
-    return this.formSubmit.hasError('passwordError');
+    return this.formSubmit.hasError("passwordError");
   }
 
-  constructor(private formBuilder: FormBuilder) {
-    this.formSubmit = this.formBuilder.group({
-      inputEmail: ['', Validators.email],
-      inputPassword: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(15)])],
-      confirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(15)])]
-    },
-    {validators: this.passwordValidator})
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.formSubmit = this.formBuilder.group(
+      {
+        inputEmail: ["", Validators.email],
+        inputPassword: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(15),
+          ]),
+        ],
+        confirmPassword: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(15),
+          ]),
+        ],
+      },
+      { validators: this.passwordValidator }
+    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   submit(): void {
-    console.log(this.formSubmit.value)
+    console.log(this.formSubmit.value);
+    this.router.navigate(["/signin"]);
   }
 
   passwordValidator(control: AbstractControl): ValidationErrors {
-    const password = control.get('inputPassword');
-    const confirm = control.get('confirmPassword');
+    const password = control.get("inputPassword");
+    const confirm = control.get("confirmPassword");
 
-    if (confirm.value !== '' && password.value !== confirm.value) {
-      return {passwordError: true}
+    if (confirm.value !== "" && password.value !== confirm.value) {
+      return { passwordError: true };
     }
     return null;
   }
-
 }
